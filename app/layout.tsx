8be2d48f88325018c1b17a5f6bad306d3556ad40
Mark <mberns006@gmail.com>
2025-07-02 11:56:37 -0700

    update text

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Metadata } from "next/dist/types";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Acme Dashboard",
-    default: "Acme Dashboard",
+    template: "%s | Demo Dashboard",
+    default: "Demo Dashboard",
   },
   description: "The official Next.js Learn Dashboard built with App Router.",
   metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
